d4a8d9dbc80756fc8f46b6bd1e574efd82fc0a82
OneManLandOnMars <peeranutac134@gmail.com>
2022-07-15 22:11:15 +0700

    Create Exerise4_Peeranut_J_2.py

---
+++ Exerise4_Peeranut_J_2.py
@@ -0,0 +1,20 @@
+English= 79.85
+General_Business=80
+Intro_Com_system = 90
+Com_Programing = 75
+
+print("-----Your Score-----")
+print("English", English, "Marks")
+print("General Business", General_Business, "marks")
+print("Introduction to Computer Systems", Intro_Com_system, "Marks")
+print("Computer Programming", Com_Programing, "Marks")
+print("--------")
+print("Thank you")
+print("See you next year")
+
+
+
+
+
+
+
